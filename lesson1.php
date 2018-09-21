b7bfcd90b275cf688016bb00885338e3d9c95230
Vika Lutsyk <vika1995109@gmail.com>
2018-09-21 16:06:53 +0300

    создан класс Одежда и добавлен конструктор

---
+++ lesson1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Class</title>
+</head>
+<body>
+    <?php
+
+    class Clothes {
+        private $name;
+        private $brand;
+        private $size;
+        private $color;
+        private $price;
+        
+        private $sale;
+        
+        public function __construct($name,$brand,$size,$color,$price, $sale) {
+            $this->name = $name;
+            $this->size = $size;
+            $this->color = $color;
+            $this->price = $price;
+            $this->sale = $sale;
+            $this->brand = $brand;
+            $this->show();
+        }
+    }
+    
+    $skirt = new Clothes("Юбка","HOME","S","red","500","10");
+    $skirt->price();
+    //$skirt->sale();
+?>
+</body>
+</html>
